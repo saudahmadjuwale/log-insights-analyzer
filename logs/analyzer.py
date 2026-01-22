6a622a4e786f54755287d98fc67b80e34be39959
saudahmadjuwale <saudjuwale26@gmail.com>
2026-01-22 10:25:42 +0000

    Added Warning Level as well

--- logs/analyzer.py
+++ logs/analyzer.py
@@ -1,9 +1,14 @@
 from collections import defaultdict
 
-error_count = 0
+errors_count = 0
 errors_by_ip = defaultdict(int)
 errors_by_message = defaultdict(int)
 errors_by_hour = defaultdict(int)
+
+warnings_count = 0
+warnings_by_ip = defaultdict(int)
+warnings_by_message = defaultdict(int)
+warnings_by_hour = defaultdict(int)
 
 def read_log_file(filepath):
     with open(filepath,"r") as file:
@@ -18,18 +23,23 @@ def parse_log_line(line):
     date,time,level,ip,message = parts
     return date,time,level,ip,message
 
-def process_error(date,time,ip,message):
-    global error_count
-    error_count += 1
-    errors_by_ip[ip] += 1
-    errors_by_message[message] += 1
-
-    hour = time.split(":")[0]
-    errors_by_hour[hour] += 1
-
+def process_errors(date,time,ip,message):
+        global errors_count
+        errors_count += 1
+        errors_by_ip[ip] += 1
+        errors_by_message[message] += 1
+        hour = time.split(":")[0]
+        errors_by_hour[hour] += 1
+def process_warnings(date,time,ip,message):
+        global warnings_count
+        warnings_count += 1
+        warnings_by_ip[ip] += 1
+        warnings_by_message[message] += 1
+        hour = time.split(":")[0]
+        warnings_by_hour[hour] += 1
 def print_summary():
     print("\n Log Analysis Summary\n")
-    print(f"Total Errors: {error_count}\n")
+    print(f"Total Errors: {errors_count} | Total Warnings: {warnings_count} \n")
 
     print("Errors By IP's:\n")
     for ip , count in errors_by_ip.items():
@@ -37,11 +47,18 @@ def print_summary():
     print("\n Errors By Messages:\n")
     for message, count in errors_by_message.items():
         print(f" {message} -> {count}")
-    
     if errors_by_hour:
         peak_hour = max(errors_by_hour,key = errors_by_hour.get)
-        print(f"\n Peakest error hour: {peak_hour}:00")
-
+        print(f"\n Peak error hour: {peak_hour}:00")
+    print("\nWarnings By IP's:\n")
+    for ip , count in warnings_by_ip.items():
+        print(f" {ip} -> {count}")
+    print("\n Warnings By Messages:\n")
+    for message, count in warnings_by_message.items():
+        print(f" {message} -> {count}")
+    if warnings_by_hour:
+        peak_hour = max(warnings_by_hour,key = warnings_by_hour.get)
+        print(f"\n Peak Warnings hour: {peak_hour}:00")
 def main():
     log_files = "sample.log"
     lines = read_log_file(log_files)
@@ -53,8 +70,9 @@ def main():
         date, time, level, ip, message = parsed 
         
         if level == "ERROR":
-            process_error(date, time, ip, message)
-    
+            process_errors(date, time, ip, message)
+        if level == "WARNING":
+            process_warnings(date,time,ip,message)
     print_summary()
 
 if __name__ == "__main__":
